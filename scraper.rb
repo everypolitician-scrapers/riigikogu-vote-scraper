7865ae5f39884309bef7721367f3d05df2d06118
Tony Bowden <tony@tmtm.com>
2014-07-22 19:40:38 +0300

    First pass at scraper

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,81 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+# Fetch voting information from riigikogu.ee
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+require 'json'
+require 'scraperwiki'
+require 'nokogiri'
+require 'open-uri'
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '/tmp/open-uri'
+
+@CALENDAR = 'http://www.riigikogu.ee/?year=2014&op=ems&page=haaletus_kalender&navbar=no&op2=print&year=%d'
+
+def vote_dates(year)
+  # Find links like ?op=ems&amp;page=haaletus_paev&amp;paev=2014-01-14&amp;
+  doc = Nokogiri::HTML(open(@CALENDAR % year).read)
+  doc.xpath("//a[contains(@href,'haaletus_paev')]").map { |link| link['href'][/paev=(\d{4}-\d{2}-\d{2})/, 1] }
+end
+
+@DAY = 'http://www.riigikogu.ee/index.php?op=ems&page=haaletus_paev&op2=print&paev=%s'
+def day_votes(date)
+  doc = Nokogiri::HTML(open(@DAY % date).read)
+  doc.css('table.List tr').drop(1).map do |tr| 
+    row = tr.css('td') 
+    link = row[0].at('a')
+    {
+      time: link.text,
+      id: link['href'][/hid=([\w\-]+)/,1],
+      sisu: row[5].text,
+      eelnõu: row[6].text,
+      eelnõu_id: ((row[6].at('a') || {})['href'] || "")[/eid=([\w\-]+)/,1],
+    }
+  end
+end
+
+@ROLL = 'http://www.riigikogu.ee/index.php?op=ems&page=haaletus&hid=%s&op2=print'
+def rollcall(id)
+  doc = Nokogiri::HTML(open(@ROLL % id).read)
+  doc.css('table.List tr').drop(1).map do |tr|
+    row = tr.css('td')
+    {
+      voteid: id,
+      voter: row[1].text,
+      option: row[2].text,
+      grouping: row[3].text,
+    }
+  end
+end
+
+def vote_option (str)
+  return 'yes' if str == 'poolt'
+  return 'no' if str == 'vastu'
+  return 'abstain' if str == 'erapooletu'
+  return 'present' if str == 'ei hääletanud'
+  return 'absent' if str == 'puudub'
+  raise "Unknown vote option: #{str}"
+end
+
+vote_dates(2014).sort.reverse.take(1).each do |day|
+  day_votes(day).compact.each do |vote|
+
+    unless (ScraperWiki.select('* FROM data WHERE voteid=?', vote[:id]).empty? rescue true) 
+      puts "Skipping #{vote[:id]}"
+      next
+    end
+
+    data = {
+      voteid: vote[:id],
+      time: "#{day} #{vote[:time]}",
+      context: vote[:eelnõu],
+      context_src: vote[:eelnõu_id],
+      classification: vote[:sisu],
+    }
+    puts "Adding #{vote[:id]}"
+    ScraperWiki.save_sqlite([:voteid], data)
+
+    rollcall = rollcall(vote[:id])
+    ScraperWiki.save_sqlite([:voteid,:voter], rollcall, 'vote')
+  end
+end
+
+
